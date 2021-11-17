c1fe79b1edc9c2e8bcdce7e28098fec1552d90cb
Dotan Gerbi <66974168+Dotan-G@users.noreply.github.com>
2021-11-17 15:18:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-function onInit() {
-    let elHeader = document.querySelector('header');
-    window.addEventListener('scroll', () => {
-        elHeader.classList.toggle('sticky', window.scrollY > 0);
+$(document).ready(() => {
+    let elHeader = $("header");
+    $(document).scroll(() => {
+        elHeader.toggleClass("sticky", window.scrollY > 0);
     });
-}
+    $(".menu-button").click(menuToggle);
+    $(".menu li a").click(() => {
+        let classList = $(".menu-button").attr("class").split(/\s+/);
+        $.each(classList, (idx, value) => {
+            if (value === "open") return menuToggle();
+        });
+    });
+    $(".owl-carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 3,
+                nav: false,
+            },
+        },
+    });
+});
 
-function onOpenMenu(elMenuButton) {
-    elMenuButton.classList.toggle('open');
-    elMenuButton.classList.toggle('close');
-    console.log(elMenuButton.classList);
-    let elMenu = document.querySelector('.menu');
-    elMenu.classList.toggle('open-menu');
-    let elBody = document.querySelector('body');
-    elBody.classList.toggle('hide-scroll');
+function menuToggle() {
+    console.log("menuToggle");
+    $(".menu-button").toggleClass("open");
+    $(".menu-button").toggleClass("close");
+    $(".menu").toggleClass("open-menu");
+    $("body").toggleClass("hide-scroll");
 }
